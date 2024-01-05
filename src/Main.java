223c7048363e4e35fb7612182742196657467721
markt33 <marktsvyan24@gmail.com>
2024-01-04 19:39:44 -0800

    Re-added the printMessage() method.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("By: Mark");
+        printMessage("Hello, world!");
+        printMessage("By: Mark");
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
     }
 }
